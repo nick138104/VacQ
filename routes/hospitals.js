99e3988616a70bd41830762337b95d24a139a85f
G10N(nick) <nick.supakorn@hotmail.com>
2023-02-11 22:45:52 +0700

    update for SW Dev Hw6

---
+++ routes/hospitals.js
@@ -0,0 +1,25 @@
+const { request } = require("express");
+const express = require("express");
+const {
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
+} = require("../controllers/hospitals");
+
+const router = express.Router();
+
+const { protect, authorize } = require("../middleware/auth");
+
+router
+  .route("/")
+  .get(getHospitals)
+  .post(protect, authorize("admin"), createHospital);
+router
+  .route("/:id")
+  .get(getHospital)
+  .put(protect, authorize("admin"), updateHospital)
+  .delete(protect, authorize("admin"), deleteHospital);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+//Route files
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  //1.  res.send('<h1>Hello from express</h1>');
+  //2.  res.send({name:'Brad'});
+  //3.  res.json({name:'Brad'});
+  //4.  res.sendStatus(400);
+  //5.  res.status(400).json({success:false});
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+
+// Connect to database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
